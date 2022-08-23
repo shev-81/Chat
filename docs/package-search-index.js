6be4daea5154f566812f586fc7b4e1e4993f7351
ANDREY SHEVELENKO <89448563+shev-81@users.noreply.github.com>
2022-08-23 17:33:34 +0300

    Delete docs directory

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.client.pak"},{"l":"com.client.pak.render"},{"l":"com.client.pak.services"},{"l":"com.server.pak"},{"l":"com.server.pak.services"},{"l":"message"}];updateSearchResults();
